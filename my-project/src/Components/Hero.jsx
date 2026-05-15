45404e4b191c67ec31e075f7e2731578b5c4f145
rexxy0011 <ifeanyiumeh47@gmail.com>
2026-05-15 09:58:27 +0100

    hero

--- my-project/src/Components/Hero.jsx
+++ my-project/src/Components/Hero.jsx
@@ -80,10 +80,10 @@ const Hero = () => {
               className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter"
             >
               Healthcare Data Analyst (MSc health Data Science) with a clinical
-              background in medical radiography with a clinical radiography
-              background. Skilled in R, SQL, Excel, and Power BI for analyzing
-              healthcare data, improving patient flow, and delivering KPI-driven
-              insights for operational efficiency and service performance.
+              background in medical radiography . Skilled in R, SQL, Excel, and
+              Power BI for analyzing healthcare data, improving patient flow,
+              and delivering KPI-driven insights for operational efficiency and
+              service performance.
             </motion.p>
 
             <motion.a
